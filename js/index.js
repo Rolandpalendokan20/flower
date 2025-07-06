9eac48a99aebb78f05418e1b4732cf728233bb93
Rolandpalendokan20 <22208029@unima.ac.id>
2025-07-06 22:28:27 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `halo mine comel, jangan lupa di buka🥰`.split('')
+const text = `halo mine comel, jangan lupa di buka`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
